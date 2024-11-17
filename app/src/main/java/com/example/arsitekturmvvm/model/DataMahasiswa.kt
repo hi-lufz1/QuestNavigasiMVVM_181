a3517a49338a120ffc5f50dbe3e0ba1bba48b1f7
hi-lufz1 <latifufauzi@gmail.com>
2024-11-18 00:28:32 +0700

    menambah data class nim dan email

--- app/src/main/java/com/example/arsitekturmvvm/model/DataMahasiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/model/DataMahasiswa.kt
@@ -2,6 +2,8 @@ package com.example.arsitekturmvvm.model
 
 data class DataMahasiswa(
     val nama: String ="",
+    val nim: String ="",
+    val email: String ="",
     val gender: String="",
     val alamat: String =""
 )
